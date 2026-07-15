6360d6dddf9c93cea58ed4ef39a69ab9cecc5809
willll <464311+willll@users.noreply.github.com>
2026-07-15 16:23:56 -0400

    refactor(main.cxx): Improve variable naming and formatting for consistency

--- Tests/src/main.cxx
+++ Tests/src/main.cxx
@@ -43,18 +43,17 @@
 #include "testsMemoryCartRam.hpp" // Include the header for memory Cart Ram tests
 #include "testsString.hpp"        // Include the header for string tests
 #include "testsSystem.hpp"        // Include the header for system tests
-#include "testsInterrupt.hpp"        // Include the header for vector tests
-#include "testsTimer.hpp"        // Include the header for vector tests
+#include "testsInterrupt.hpp"     // Include the header for vector tests
+#include "testsTimer.hpp"         // Include the header for vector tests
 
 // Using to shorten names for Vector and HighColor
 using namespace SRL::Types;
 using namespace SRL::Math::Types;
 using namespace SRL::Logger;
 
-
 // Define tags for test start and end
-const char *const strStart = "***UT_START***";
-const char *const strEnd = "***UT_END***";
+const char *const StrStart = "***UT_START***";
+const char *const StrEnd = "***UT_END***";
 
 /**
  * Main program entry
@@ -66,165 +65,164 @@ const char *const strEnd = "***UT_END***";
  */
 int main()
 {
-  SRL::Input::Digital pad(0);
-  size_t start_index = 0;
+    SRL::Input::Digital pad(0);
+    size_t startIndex = 0;
 
-  // Initialize SRL core with a high color
-  SRL::Core::Initialize(HighColor(20, 10, 50));
-  ASCII::Clear();
+    // Initialize SRL core with a high color
+    SRL::Core::Initialize(HighColor(20, 10, 50));
+    ASCII::Clear();
 
-  // Tag the beginning of the tests
-  LogInfo(strStart);
+    // Tag the beginning of the tests
+    LogInfo(StrStart);
 
-  PushResultLine(strStart);
+    PushResultLine(StrStart);
 
-  // RUN_AND_DISPLAY_SUITE(aabb_test_suite);
-  // RUN_AND_DISPLAY_SUITE(angle_test_suite);
-  RUN_AND_DISPLAY_SUITE(ascii_test_suite);
+    // RUN_AND_DISPLAY_SUITE(aabb_test_suite);
+    // RUN_AND_DISPLAY_SUITE(angle_test_suite);
+    RUN_AND_DISPLAY_SUITE(ascii_test_suite);
 
-  // Run angle test suite
-  RUN_AND_DISPLAY_SUITE(angle_test_suite);
+    // Run angle test suite
+    RUN_AND_DISPLAY_SUITE(angle_test_suite);
 
-  // Run CD test suite
-  RUN_AND_DISPLAY_SUITE(cd_test_suite);
+    // Run CD test suite
+    RUN_AND_DISPLAY_SUITE(cd_test_suite);
 
-  // Run CRAM test suite
-  RUN_AND_DISPLAY_SUITE(cram_test_suite);
+    // Run CRAM test suite
+    RUN_AND_DISPLAY_SUITE(cram_test_suite);
 
-  // Run FXP test suite
-  RUN_AND_DISPLAY_SUITE(fxp_test_suite);
+    // Run FXP test suite
+    RUN_AND_DISPLAY_SUITE(fxp_test_suite);
 
-  // Run HighColor test suite
-  RUN_AND_DISPLAY_SUITE(highcolor_test_suite);
+    // Run HighColor test suite
+    RUN_AND_DISPLAY_SUITE(highcolor_test_suite);
 
-  // Run Math test suite
-  RUN_AND_DISPLAY_SUITE(math_test_suite);
+    // Run Math test suite
+    RUN_AND_DISPLAY_SUITE(math_test_suite);
 
-  // Run Memory test suite
-  //RUN_AND_DISPLAY_SUITE(memory_test_suite);
+    // Run Memory test suite
+    // RUN_AND_DISPLAY_SUITE(memory_test_suite);
 
-  // Run Base test suite (SGL)
-  RUN_AND_DISPLAY_SUITE(base_test_suite);
+    // Run Base test suite (SGL)
+    RUN_AND_DISPLAY_SUITE(base_test_suite);
 
-  // Run Bitmap test suite
-  RUN_AND_DISPLAY_SUITE(bitmap_test_suite);
+    // Run Bitmap test suite
+    RUN_AND_DISPLAY_SUITE(bitmap_test_suite);
 
-  // Run Memory HWRam test suite
-  RUN_AND_DISPLAY_SUITE(memory_HWRam_test_suite);
-  RUN_AND_DISPLAY_SUITE(memory_LWRam_test_suite);
+    // Run Memory HWRam test suite
+    RUN_AND_DISPLAY_SUITE(memory_HWRam_test_suite);
+    RUN_AND_DISPLAY_SUITE(memory_LWRam_test_suite);
 
-  // Run Memory CartRam test suite
-  //RUN_AND_DISPLAY_SUITE(memory_CartRam_test_suite);
+    // Run Memory CartRam test suite
+    // RUN_AND_DISPLAY_SUITE(memory_CartRam_test_suite);
 
-  // Run Interrupt test suite
-  RUN_AND_DISPLAY_SUITE(interrupt_test_suite);
+    // Run Interrupt test suite
+    RUN_AND_DISPLAY_SUITE(interrupt_test_suite);
 
-  // Run System test suite
-  //RUN_AND_DISPLAY_SUITE(system_test_suite);
+    // Run System test suite
+    // RUN_AND_DISPLAY_SUITE(system_test_suite);
 
-  // Run Timer test suite
-  RUN_AND_DISPLAY_SUITE(test_timer_suite);
+    // Run Timer test suite
+    RUN_AND_DISPLAY_SUITE(test_timer_suite);
 
-  // Generate tests report
-  MU_REPORT();
+    // Generate tests report
+    MU_REPORT();
 
-  // Display test statistics
-  BuildStatsLine(results_buffer, kBufferSize,
-                 static_cast<unsigned int>(minunit_run),
-                 static_cast<unsigned int>(minunit_assert),
-                 static_cast<unsigned int>(minunit_fail));
+    // Display test statistics
+    BuildStatsLine(resultsBuffer, BufferSize,
+        static_cast<unsigned int>(minunit_run),
+        static_cast<unsigned int>(minunit_assert),
+        static_cast<unsigned int>(minunit_fail));
 
-  PushResultLine(results_buffer);
+    PushResultLine(resultsBuffer);
 
-  // Tag the end of the tests
-  LogInfo(strEnd);
-  PushResultLine(strEnd);
+    // Tag the end of the tests
+    LogInfo(StrEnd);
+    PushResultLine(StrEnd);
 
-  if (g_results.size() > kDisplayLines)
-  {
-    start_index = g_results.size() - kDisplayLines;
-  }
-
-  RenderResults(start_index);
-
-  // Main program loop
-  uint8_t up_hold_frames = 0;
-  uint8_t down_hold_frames = 0;
-  const uint8_t repeat_delay = 20;
-  const uint8_t repeat_rate = 3;
-
-  while (1)
-  {
-    SRL::Core::Synchronize();
-    bool refresh = false;
-
-    const bool up_held = pad.IsHeld(SRL::Input::Digital::Button::Up);
-    const bool down_held = pad.IsHeld(SRL::Input::Digital::Button::Down);
-
-    if (up_held && !down_held)
+    if (gResults.size() > DisplayLines)
     {
-      if (up_hold_frames < 255)
-      {
-        ++up_hold_frames;
-      }
-      down_hold_frames = 0;
-    }
-    else if (down_held && !up_held)
-    {
-      if (down_hold_frames < 255)
-      {
-        ++down_hold_frames;
-      }
-      up_hold_frames = 0;
-    }
-    else
-    {
-      up_hold_frames = 0;
-      down_hold_frames = 0;
+        startIndex = gResults.size() - DisplayLines;
     }
 
-    if (pad.WasPressed(SRL::Input::Digital::Button::Up))
+    RenderResults(startIndex);
+
+    // Main program loop
+    uint8_t upHoldFrames = 0;
+    uint8_t downHoldFrames = 0;
+    const uint8_t repeatDelay = 20;
+    const uint8_t repeatRate = 3;
+
+    while (1)
     {
-      if (start_index > 0)
-      {
-        --start_index;
-        refresh = true;
-      }
-    }
-    else if (up_held && up_hold_frames > repeat_delay &&
-             ((up_hold_frames - repeat_delay) % repeat_rate == 0))
-    {
-      if (start_index > 0)
-      {
-        --start_index;
-        refresh = true;
-      }
+        SRL::Core::Synchronize();
+        bool refresh = false;
+
+        const bool upHeld = pad.IsHeld(SRL::Input::Digital::Button::Up);
+        const bool downHeld = pad.IsHeld(SRL::Input::Digital::Button::Down);
+
+        if (upHeld && !downHeld)
+        {
+            if (upHoldFrames < 255)
+            {
+                ++upHoldFrames;
+            }
+            downHoldFrames = 0;
+        }
+        else if (downHeld && !upHeld)
+        {
+            if (downHoldFrames < 255)
+            {
+                ++downHoldFrames;
+            }
+            upHoldFrames = 0;
+        }
+        else
+        {
+            upHoldFrames = 0;
+            downHoldFrames = 0;
+        }
+
+        if (pad.WasPressed(SRL::Input::Digital::Button::Up))
+        {
+            if (startIndex > 0)
+            {
+                --startIndex;
+                refresh = true;
+            }
+        }
+        else if (upHeld && upHoldFrames > repeatDelay &&
+                 ((upHoldFrames - repeatDelay) % repeatRate == 0))
+        {
+            if (startIndex > 0)
+            {
+                --startIndex;
+                refresh = true;
+            }
+        }
+
+        if (pad.WasPressed(SRL::Input::Digital::Button::Down))
+        {
+            if (startIndex + DisplayLines < gResults.size())
+            {
+                ++startIndex;
+                refresh = true;
+            }
+        }
+        else if (downHeld && downHoldFrames > repeatDelay &&
+                 ((downHoldFrames - repeatDelay) % repeatRate == 0))
+        {
+            if (startIndex + DisplayLines < gResults.size())
+            {
+                ++startIndex;
+                refresh = true;
+            }
+        }
+
+        if (refresh)
+        {
+            RenderResults(startIndex);
+        }
     }
 
-    if (pad.WasPressed(SRL::Input::Digital::Button::Down))
-    {
-      if (start_index + kDisplayLines < g_results.size())
-      {
-        ++start_index;
-        refresh = true;
-      }
-    }
-    else if (down_held && down_hold_frames > repeat_delay &&
-             ((down_hold_frames - repeat_delay) % repeat_rate == 0))
-    {
-      if (start_index + kDisplayLines < g_results.size())
-      {
-        ++start_index;
-        refresh = true;
-      }
-    }
-
-    if (refresh)
-    {
-      RenderResults(start_index);
-    }
-
-  }
-
-  return 0;
+    return 0;
 }
